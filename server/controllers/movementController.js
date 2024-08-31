1fdee471b49afdb92452aaca4fb75432ad123246
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-08-31 18:28:34 -0500

    feat: :sparkles: Implement booking functionality for ticket seats
    This commit adds the feature to book seats in a ticket without the need for immediate payment.

--- server/controllers/movementController.js
+++ server/controllers/movementController.js
@@ -197,3 +197,92 @@ exports.purchaseTicket = async (req, res) => {
     payment: new PaymentDTO(payment),
   });
 };
+
+/**
+ * @function bookTicket
+ * @description Book a ticket for a show. If the seats are available, the ticket is booked. If not, the booking is put on hold.
+ * @async
+ * @method
+ * @param {Object} req - HTTP request object containing the ticket data in `req.body`.
+ * @param {Object} res - HTTP response object to send the response to the client.
+ * @returns {void}
+ * @throws {Error} Throws an error if the ticket booking fails.
+ * @response {Object} Responds with the booked ticket if successful.
+ * @response {Object} Responds with an error message if the booking fails.
+ */
+exports.bookTicket = async (req, res) => {
+  const {
+    user_id,
+    show_id,
+    date_movement,
+    seats,
+    description,
+  } = req.body;
+
+  const { result: user, err: userErr } = await handleAsync(() => User.findById(user_id));
+  if (userErr || !user) {
+    return res.status(404).json({ message: "User not found." });
+  }
+
+  const { result: show, err: showErr } = await handleAsync(() => Show.findById(show_id));
+  if (showErr || !show) {
+    return res.status(404).json({ message: "Show not found." });
+  }
+
+  const unavailableSeats = seats.filter((seat) => {
+    const seatShow = show.available_seats.find((s) => s.seat === seat);
+    return seatShow && !seatShow.availability;
+  });
+
+  if (unavailableSeats.length > 0) {
+    const movementOnHold = new Movement({
+      user_id,
+      show_id,
+      date_movement,
+      status: "on Hold",
+      seats,
+      description,
+    });
+
+    const { err: movementSaveErr } = await handleAsync(() => movementOnHold.save());
+    if (movementSaveErr) {
+      return res.status(500).json({ message: "Error creating movement." });
+    }
+
+    return res.status(200).json({
+      message: `The following seats are not available: ${unavailableSeats.join(", ")}. Your booking is on hold.`,
+      ticket: new MovementDTO(movementOnHold),
+    });
+  } else {
+    show.available_seats.forEach((seatShow) => {
+      if (seats.includes(seatShow.seat)) {
+        seatShow.availability = false;
+      }
+    });
+    
+    const { err: showSaveErr } = await handleAsync(() => show.save());
+    if (showSaveErr) {
+      return res.status(500).json({ message: "Error updating show seats." });
+    }
+
+    const movementBooked = new Movement({
+      user_id,
+      show_id,
+      date_movement,
+      status: "booked",
+      seats,
+      description,
+    });
+
+
+    const { err: movementSaveErr } = await handleAsync(() => movementBooked.save());
+    if (movementSaveErr) {
+      return res.status(500).json({ message: "Error creating movement." });
+    }
+
+    return res.status(201).json({
+      message: "Ticket booked successfully.",
+      ticket: new MovementDTO(movementBooked),
+    });
+  }
+}
--- server/routes/movementRoutes.js
+++ server/routes/movementRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { purchaseTicket } = require("../controllers/movementController");
+const { purchaseTicket, bookTicket } = require("../controllers/movementController");
 const { validate } = require ("../middleware/validationMiddleware");
 const { movementValidator } = require("../validators/movementValidator");
 const router = express.Router();
 
 router.post('/purchase/v1', purchaseTicket, validate, movementValidator());
+router.post('/book/v1', bookTicket, validate, movementValidator());
 
 module.exports = router;
