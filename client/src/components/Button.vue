ef513783a737993748819b63ce455fc23316d28b
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-09-23 13:58:51 -0500

    feat: :heavy_plus_sign: Add Vue and Tailwind Dependencies
    This commit introduces Vue and Tailwind CSS to the project. It also includes the creation of the initial Home view.

---
+++ client/src/components/Button.vue
@@ -0,0 +1,27 @@
+<template>
+  <button
+    :class="['font-inter font-semibold rounded-lg', buttonClass]"
+    @click="$emit('click')"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    buttonClass: {
+      type: String,
+      default: 'bg-slate-950 font-inter font-semibold text-slate-50 px-36 py-4 rounded-lg mb-3.5' 
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
