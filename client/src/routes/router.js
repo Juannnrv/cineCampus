9a8583eb9f0dc9cf1512690436161fcb1e379603
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-09-24 08:32:17 -0500

    feat: :sparkles: Add cinema view style
    Enhanced home page with styled cinema view components

--- client/src/routes/router.js
+++ client/src/routes/router.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import CreateAccount from '../views/CreateAccount.vue'
 import Login from '../views/Login.vue'
+import Cine from '../views/Cinema.vue'
 
 const routes = [
     { path: '/', component: Home },
     { path: '/createAccount', component: CreateAccount },
     { path: '/login', component: Login },
+    { path: '/cine', component: Cine },
 ]
 
 const router = createRouter({
---
+++ client/src/views/Cinema.vue
@@ -0,0 +1,40 @@
+<template>
+  <section
+    class="h-screen w-screen flex flex-col justify-center items-center bg-color-1"
+  >
+    <nav class="mx-8 mt-14 flex gap-3">
+      <div class="flex gap-3">
+        <div class="relative">
+          <img
+            class="w-10 h-10 rounded-full"
+            src="../assets/img/gengar.png"
+            alt=""
+          />
+          <span
+            class="top-0 left-7 absolute w-3.5 h-3.5 bg-green-400 border-2 border-white dark:border-gray-800 rounded-full"
+          ></span>
+        </div>
+        <div class="">
+          <p class="font-inter font-regular text-color-3">Hi, Gengar</p>
+          <p class="font-inter font-bold text-color-3">
+            Let’s watch movie together!
+          </p>
+        </div>
+      </div>
+      <img class="w-10 border-2 rounded-md border-color-4" :src="notificationImg" />
+    </nav>
+  </section>
+</template>
+
+<script>
+import notificationImg from "../assets/img/notification.svg";
+
+export default {
+  name: "Cinema",
+  data() {
+    return {
+      notificationImg
+    };
+  },
+};
+</script>
