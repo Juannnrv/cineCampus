e27b5f5060022f395cd197a8bfea229dd7916916
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-09-26 09:07:11 -0500

    feat: :sparkles: Implement cookie-based authentication and redirection logic
    Updated `authController.js` to set a secure, httpOnly cookie upon successful login, modified `Login.vue` to handle cookie creation and validation, added logic in `Cinema.vue` to check for the presence of a valid cookie and redirect to the login page if the cookie is not found and ensured that users without a valid cookie cannot access the movie viewing functionality.

--- client/src/views/Cinema.vue
+++ client/src/views/Cinema.vue
@@ -145,12 +145,16 @@ export default {
     this.getMovies();
   },
   methods: {
+    getCookie(name) {
+      const value = `; ${document.cookie}`;
+      const parts = value.split(`; ${name}=`);
+      if (parts.length === 2) return parts.pop().split(";").shift();
+    },
     async getMovies() {
-      const token = sessionStorage.getItem("token");
+      const token = this.getCookie("token");
       if (!token) {
-        sessionStorage.removeItem("token");
-        console.error("No token found, redirecting to home");
-        this.$router.push("/");
+        console.error("No token found");
+        this.goToLogin();
         return;
       }
       try {
@@ -183,11 +187,14 @@ export default {
     selectedMovie(id) {
       this.$router.push(`/detail/${id}`);
     },
+    goToLogin() {
+      this.$router.push("/login");
+    },
   },
 };
 </script>
 
-<style scoped>
+<style>
 @import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap");
 @import url("https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap");
 
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -24,8 +24,6 @@ const loginUser = async (req, res) => {
 
     res.cookie("token", token, { 
       maxAge: 3600000,
-      httpOnly: true, 
-      secure: false 
     });
 
     res.status(200).json({ message: "Logged in", token: token });
